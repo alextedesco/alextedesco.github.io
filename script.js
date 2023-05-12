bdde5264855a2cccdd92c9a1953ea09c3af1e075
Alex Tedesco <act2076@rit.edu>
2023-05-12 00:05:53 -0400

    JavaScript that smoothly goes to section when clicked in nav

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ document.addEventListener('DOMContentLoaded', function() {
       progressBar.style.width = width + '%';
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const headerHeight = document.querySelector('header').offsetHeight;
+  
+    // Scroll to section and adjust for fixed header
+    function scrollToSection(event) {
+      event.preventDefault();
+      const targetId = this.getAttribute('href');
+      const targetPosition = targetId === '#' ? 0 : document.querySelector(targetId).offsetTop - headerHeight;
+  
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    }
+  
+    // Attach click event to each navigation link
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(function(link) {
+      link.addEventListener('click', scrollToSection);
+    });
+  });
